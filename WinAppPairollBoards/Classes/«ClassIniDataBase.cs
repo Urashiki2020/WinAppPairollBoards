ac7f4d79cdfd448c77e2a52f450432dddcc1b4d8
Urashiki2020 <yungxrist1995@mail.ru>
2020-04-12 00:11:12 +0300

    Инициализация базы данных

---
+++ WinAppPairollBoards/Classes/«ClassIniDataBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient; //данный модуль необходимо подключить самостоятельно 
+using System.Data.Odbc; //данный модуль необходимо подключить самостоятельно 
+
+namespace WinAppPairollBoards
+{
+    class _ClassIniDataBase
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-TVLAIMU\SQLEXPRESS;Initial Catalog=PairollBoards;Integrated Security=True"); //Строка подключения базы данных
+
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+        public SqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
