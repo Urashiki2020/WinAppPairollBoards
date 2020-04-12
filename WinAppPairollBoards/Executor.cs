42255938a9203114618a8a62ea44577c0fc242b0
Urashiki2020 <yungxrist1995@mail.ru>
2020-04-12 16:48:19 +0300

    Окно авторизации мнеджеров и исполнителей.

--- WinAppPairollBoards/Executor.cs
+++ WinAppPairollBoards/Executor.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WinAppPairollBoards
 {
-    public partial class Form1 : Form
+    public partial class FormExecutor : Form
     {
-        public Form1()
+        public FormExecutor()
         {
             InitializeComponent();
         }
