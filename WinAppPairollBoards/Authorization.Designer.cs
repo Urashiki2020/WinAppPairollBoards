45fcc191279a52844da1c7852539345996438b42
Urashiki2020 <yungxrist1995@mail.ru>
2020-04-12 16:48:19 +0300

    Окно авторизации менеджеров и исполнителей.

---
+++ WinAppPairollBoards/Authorization.Designer.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace WinAppPairollBoards
+{
+    partial class FormAuthorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAuthorization));
+            this.labelGreeting = new System.Windows.Forms.Label();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.pictureBoxEsoft = new System.Windows.Forms.PictureBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonInput = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEsoft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelGreeting
+            // 
+            this.labelGreeting.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelGreeting.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelGreeting.Location = new System.Drawing.Point(61, 148);
+            this.labelGreeting.Name = "labelGreeting";
+            this.labelGreeting.Size = new System.Drawing.Size(691, 55);
+            this.labelGreeting.TabIndex = 0;
+            this.labelGreeting.Text = "Добро пожаловать в систему автоматизации расчета заработной платы.\r\nВведите логин" +
+    " и пароль для входа в систему";
+            this.labelGreeting.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLogin.Location = new System.Drawing.Point(209, 224);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(141, 24);
+            this.labelLogin.TabIndex = 1;
+            this.labelLogin.Text = "Введите логин:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPassword.Location = new System.Drawing.Point(198, 271);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(152, 24);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Введите пароль:";
+            this.labelPassword.Click += new System.EventHandler(this.labelPassword_Click);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxLogin.Location = new System.Drawing.Point(356, 227);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(192, 22);
+            this.textBoxLogin.TabIndex = 3;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxPassword.Location = new System.Drawing.Point(356, 273);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(192, 22);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // pictureBoxEsoft
+            // 
+            this.pictureBoxEsoft.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxEsoft.Image")));
+            this.pictureBoxEsoft.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxEsoft.Name = "pictureBoxEsoft";
+            this.pictureBoxEsoft.Size = new System.Drawing.Size(247, 97);
+            this.pictureBoxEsoft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxEsoft.TabIndex = 6;
+            this.pictureBoxEsoft.TabStop = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonExit.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExit.Location = new System.Drawing.Point(437, 319);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(111, 44);
+            this.buttonExit.TabIndex = 7;
+            this.buttonExit.Text = "Выход";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonInput
+            // 
+            this.buttonInput.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonInput.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonInput.Location = new System.Drawing.Point(202, 319);
+            this.buttonInput.Name = "buttonInput";
+            this.buttonInput.Size = new System.Drawing.Size(111, 44);
+            this.buttonInput.TabIndex = 8;
+            this.buttonInput.Text = "Вход";
+            this.buttonInput.UseVisualStyleBackColor = true;
+            this.buttonInput.Click += new System.EventHandler(this.buttonInput_Click_1);
+            // 
+            // FormAuthorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 375);
+            this.Controls.Add(this.buttonInput);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.pictureBoxEsoft);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.labelGreeting);
+            this.Name = "FormAuthorization";
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.FormAuthorization_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEsoft)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void buttonInput_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelGreeting;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.PictureBox pictureBoxEsoft;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonInput;
+    }
+}
+
